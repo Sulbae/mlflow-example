128533f395f5fff2b815a7cc4bfe84a0644c8596
Sulbae <assulis500@gmail.com>
2025-11-16 12:05:08 +0700

    update modelling.py

--- Latihan-CI/MLproject/modelling.py
+++ Latihan-CI/MLproject/modelling.py
@@ -14,6 +14,8 @@ dagshub.init(
 )
 
 mlflow.set_experiment("Latihan Credit Scoring")
+# Aktifkan autolog
+mlflow.autolog(log_models=False)
 
 data = pd.read_csv("train_pca.csv")
 
@@ -24,28 +26,31 @@ X_train, X_test, y_train, y_test = train_test_split(
     random_state=42
 )
 
-input_example = X_train[0:5]
+input_example = X_train.iloc[0:5]
 
 with mlflow.start_run():
+    
     # Log parameters
     n_estimators = 505
     max_depth = 37
-    mlflow.autolog()
 
+    mlflow.log_param("n_estimators", n_estimators)
+    mlflow.log_param("max_depth", max_depth)
+    
     # Train model
     model = RandomForestClassifier(
         n_estimators=n_estimators,
         max_depth=max_depth
     )
-
-    mlflow.sklearn.log_model(
-        sk_model=model,
-        artifact_path="model",
-        input_example=input_example
-    )
-
     model.fit(X_train, y_train)
 
     # Log metrics
     accuracy = model.score(X_test, y_test)
     mlflow.log_metric("accuracy", accuracy)
+
+    # Log model
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="model",
+        input_example=input_example
+    )
