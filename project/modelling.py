a81e52e2a569f66be3006bf627c58e6f3dcd55bd
Sulbae <assulis500@gmail.com>
2025-11-16 12:48:33 +0700

    test

--- project/modelling.py
+++ project/modelling.py
@@ -17,7 +17,7 @@ mlflow.set_experiment("Latihan Credit Scoring")
 # Aktifkan autolog
 mlflow.autolog(log_models=False)
 
-data = pd.read_csv("train_pca.csv"
+data = pd.read_csv("train_pca.csv")
 
 X_train, X_test, y_train, y_test = train_test_split(
     data.drop("Credit_Score", axis=1), 
